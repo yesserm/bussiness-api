44971460a81e4882de9927f45b9cce30507920d6
yesserm <yesser.miranda0106@gmail.com>
2026-07-07 15:13:22 -0600

    Initial project base code to spring boot

---
+++ src/test/java/dev/yesserm/demosb4/Demosb4ApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.yesserm.demosb4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Demosb4ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
