379afdd331f4072b09746b11ef77a9c86b7fa4bd
yesserm <yesser.miranda0106@gmail.com>
2026-07-09 10:56:29 -0600

    feat(discovery) - Add Eureka server

---
+++ platform/eureka-server/src/test/java/dev/yesserm/demosb4/discovery/EurekaServerApplicationTests.java
@@ -0,0 +1,12 @@
+package dev.yesserm.demosb4.discovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
