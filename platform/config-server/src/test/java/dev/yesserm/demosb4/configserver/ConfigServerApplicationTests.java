aac9c44de0a8614abfc29dbae81fd36dbcacc021
yesserm <yesser.miranda0106@gmail.com>
2026-07-09 11:02:01 -0600

    feat(config) - Add centralized configuration server

---
+++ platform/config-server/src/test/java/dev/yesserm/demosb4/configserver/ConfigServerApplicationTests.java
@@ -0,0 +1,12 @@
+package dev.yesserm.demosb4.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
